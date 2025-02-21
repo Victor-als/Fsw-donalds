5cb2b15d71a033b7220d811fb14991d9feb9d7f6
Victor-als <victor.ccx814@gmail.com>
2025-02-21 14:50:05 -0300

    feat: add initial cart product item component (without functionality)

---
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -0,0 +1,61 @@
+import { ChevronLeftIcon, ChevronRightIcon, Trash2Icon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartProduct } from "../contexts/cart";
+
+interface CartItemProductProps {
+  product: CartProduct;
+}
+
+const CartProductItem = ({ product }: CartItemProductProps) => {
+  return ( 
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <div className="relative h-20 w-20  bg-gray-100 rounded-xl">
+          <Image src={product.imageUrl} alt={product.name} fill/>
+        </div>
+
+        <div className="space-y-1">
+
+          <p className="max-w-[90%] text-xs truncate text-ellipsis">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
+          
+          <div className=" flex items-center gap-1 text-center">
+            <Button 
+              variant="outline"
+              className="h-7 rounded-lg w-7"
+              
+            >
+              <ChevronLeftIcon size={16}/>
+            </Button>
+            <p className="w-7 text-xs">{product.quantity}</p>
+            <Button 
+              variant="destructive"
+              className="rounded-lg h-7 w-7"
+              
+            >
+              <ChevronRightIcon size={16}/>
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <Button 
+        className="h-7 w-7 rounded-lg" 
+        variant="outline"
+      >
+        <Trash2Icon />
+      </Button>
+    </div>
+
+   );
+}
+ 
+export default CartProductItem;
