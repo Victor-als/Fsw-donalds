42466196d8b83c1e9816209911582b361907ec1e
Victor-als <victor.ccx814@gmail.com>
2025-02-21 12:09:21 -0300

    feat: ensure same product can be added more than onnce to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct 
- extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
 
@@ -20,29 +20,44 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
-})
- 
+});
 
-export const CartProvider = ({ children }: { children: ReactNode}) => {
-  const [products, setProducts] = useState<CartProduct[]>([])
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleCart = () => {
-    setIsOpen(prev => !prev)
-  }
-
+    setIsOpen((prev) => !prev);
+  };
   const addProduct = (product: CartProduct) => {
-    products.push(product);
-    setProducts((prev) => [...prev, product])
-  }
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
   return (
-    <CartContext.Provider value={{
-      isOpen,
-      products,
-      toggleCart,
-      addProduct,
-    }}>
+    <CartContext.Provider
+      value={{
+        isOpen,
+        products,
+        toggleCart,
+        addProduct,
+      }}
+    >
       {children}
     </CartContext.Provider>
-  )
-}
+  );
+};
